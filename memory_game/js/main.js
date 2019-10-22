2a4b693e2db9b95328e27d7afd24167902d5e910
Nikita Lukjanov <nlukjanov@gmail.com>
2019-10-22 11:49:11 +0300

    added more card, reset button and player score

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -17,6 +17,26 @@ var cards = [{
   "rank": "king",
   "suit": "diamonds",
   "cardImage": "images/king-of-diamonds.png"
+},
+{
+  "rank": "queen",
+  "suit": "hearts",
+  "cardImage": "images/queen-of-hearts.png"
+},
+{
+  "rank": "queen",
+  "suit": "diamonds",
+  "cardImage": "images/queen-of-diamonds.png"
+},
+{
+  "rank": "king",
+  "suit": "hearts",
+  "cardImage": "images/king-of-hearts.png"
+},
+{
+  "rank": "king",
+  "suit": "diamonds",
+  "cardImage": "images/king-of-diamonds.png"
 }];
 var cardsInPlay = [];
 
@@ -34,12 +54,12 @@ function createBoard() {
 function flipCard() {
   var cardId = this.getAttribute('data-id');
   
-  console.log("User flipped " + cards[cardId].rank);
+  // console.log("User flipped " + cards[cardId].rank);
+  // console.log("User flipped " + cards[cardId].cardImage);
+  // console.log("User flipped " + cards[cardId].suit);
 
   cardsInPlay.push(cards[cardId].rank);
 
-  console.log("User flipped " + cards[cardId].cardImage);
-  console.log("User flipped " + cards[cardId].suit);
   this.setAttribute('src', cards[cardId].cardImage);
 
   function checkForMatch() {
