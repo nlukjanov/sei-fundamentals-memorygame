7503d8e3effab20f06fae5e47aca3af59019938d
Nikita Lukjanov <nlukjanov@gmail.com>
2019-10-23 00:04:59 +0300

    added reset functionality and score tracking

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -17,6 +17,26 @@ var cards = [{
   "rank": "king",
   "suit": "diamonds",
   "cardImage": "images/king-of-diamonds.png"
+},
+{
+  "rank": "queen",
+  "suit": "hearts",
+  "cardImage": "images/queen-of-hearts.png"
+},
+{
+  "rank": "queen",
+  "suit": "diamonds",
+  "cardImage": "images/queen-of-diamonds.png"
+},
+{
+  "rank": "king",
+  "suit": "hearts",
+  "cardImage": "images/king-of-hearts.png"
+},
+{
+  "rank": "king",
+  "suit": "diamonds",
+  "cardImage": "images/king-of-diamonds.png"
 }];
 var cardsInPlay = [];
 
@@ -31,6 +51,32 @@ function createBoard() {
   }
 }
 
+var resetButton = document.getElementById('reset-button');
+resetButton.addEventListener('click', resetGame);
+
+var score = 0;
+var notification = document.getElementById('notification');
+var playerScore = document.getElementById('player-score');
+var node = document.getElementById('game-board');
+
+function newBoard() {
+  cardsInPlay = [];
+  node.innerHTML = "";
+  createBoard();
+}
+
+function notificationReset() {
+  notification.style.color = "white";
+  notification.innerHTML = ' ';
+}
+
+function resetGame() {
+  score = 0;
+  playerScore.innerHTML = score;
+  notificationReset();
+  newBoard();
+}
+
 function flipCard() {
 
   var cardId = this.getAttribute('data-id');
@@ -40,24 +86,32 @@ function flipCard() {
   function checkForMatch() {
     
     if (cardsInPlay.length === 2) {
-      var score = 0;
-      var notification = document.getElementById('notification');
-      var playerScore = document.getElementById('player-score');
-      
       if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+        notification.style.color = "green";
         notification.innerHTML = 'It is a match!';
         score = score + 1;
         playerScore.innerHTML = (score);
-      } else {
-        notification.innerHTML = 'Try again!';        
         var cardsToFlipBack = document.querySelectorAll('img');
         setTimeout(function() {
           for (let i = 0; i < cardsToFlipBack.length; i++) {
             cardsToFlipBack[i].setAttribute('src', 'images/back.png');
+            notificationReset();
           };
-        }, 3000);
+        }, 1000);
+      } else {
+        notification.style.color = "red";
+        notification.innerHTML = 'Try again!';
+        var cardsToFlipBack = document.querySelectorAll('img');
+        setTimeout(function() {
+          for (let i = 0; i < cardsToFlipBack.length; i++) {
+            cardsToFlipBack[i].setAttribute('src', 'images/back.png');
+            notificationReset();
+          };
+        }, 1000);
       }
+      setTimeout(newBoard, 1000);
     }
+    
   }
   checkForMatch();
 }
